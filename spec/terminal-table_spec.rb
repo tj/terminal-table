60dea31bf79532cd837eab41c01d5329f288d3c9
TJ Holowaychuk <tj@vision-media.ca>
2009-01-07 16:09:33 -0800

    - Most of the spec ready to go

--- spec/terminal-table_spec.rb
+++ spec/terminal-table_spec.rb
@@ -6,23 +6,107 @@ describe TerminalTable do
   end
   
   it "should render properly" do
-    @table << ['Character', 'Number']
+    @table.headings = ['Char', 'Num']
     @table << ['a', 1]
     @table << ['b', 2]
     @table << ['c', 3]
     @table.render.should == <<-EOF.deindent
-      +--------------------+
-      | Character | Number |
-      +-----------+--------+
-      | a         | 1      |
-      | b         | 2      |
-      | c         | 3      |
-      +-----------+--------+
+      +------+-----+
+      | Char | Num |
+      +------+-----+
+      | a    | 1   |
+      | b    | 2   |
+      | c    | 3   |
+      +------+-----+
+    EOF
+  end
+  
+  it "should render properly without headings" do
+    @table << ['a', 1]
+    @table << ['b', 2]
+    @table << ['c', 3]
+    @table.render.should == <<-EOF.deindent
+      +------+-----+
+      | a    | 1   |
+      | b    | 2   |
+      | c    | 3   |
+      +------+-----+
+    EOF
+  end
+  
+  it "should render properly without headings and row seperators" do
+    @table << ['a', 1]
+    @table << ['b', 2]
+    @table << ['c', 3]
+    @table.render.should == <<-EOF.deindent
+      +------+-----+
+      | a    | 1   |
+      +------+-----+
+      | b    | 2   |
+      +------+-----+
+      | c    | 3   |
+      +------+-----+
     EOF
   end
   
   it "should allows a hash of options for creation" do
-
+    headings = ['Char', 'Num']
+    rows = [['a', 1], ['b', 2], ['c', 3]]
+    TerminalTable.new(:rows => rows, :headings => headings).render.should == <<-EOF.deindent
+      +------+-----+
+      | Char | Num |
+      +------+-----+
+      | a    | 1   |
+      | b    | 2   |
+      | c    | 3   |
+      +------+-----+
+    EOF
+  end
+  
+  it "should flex for large cells" do
+    @table.headings = ['Just some characters', 'Num']
+    @table.rows = [['a', 1], ['b', 2], ['c', 3]]
+    @table.render.should == <<-EOF.deindent
+      +----------------------+-----+
+      | Just some characters | Num |
+      +----------------------+-----+
+      | a                    | 1   |
+      | b                    | 2   |
+      | c                    | 3   |
+      +----------------------+-----+
+    EOF
+  end
+  
+  it "should allow alignment of headings and cells" do
+    @table.headings = ['Characters', { :value => 'N', :align => :right }]
+    @table << [{ :value => 'a', :align => :center }, 1]
+    @table << ['b', 2]
+    @table << ['c', 3]
+    @table.render.should == <<-EOF.deindent
+      +------------+-----+
+      | Characters |   N |
+      +------------+-----+
+      |     a      | 1   |
+      | b          | 2   |
+      | c          | 3   |
+      +------------+-----+
+    EOF
+  end
+  
+  it "should align columns" do
+    @table.headings = ['Just some characters', 'Num']
+    @table.rows = [[{ :value => 'a', :align => :center }, 1], ['b', 2], ['c', 3]]
+    @table.align_column 1, :center
+    @table.align_column 2, :right
+    @table.render.should == <<-EOF.deindent
+      +----------------------+-----+
+      | Just some characters | Num |
+      +----------------------+-----+
+      |          a           |  1  |
+      |          b           |   2 |
+      |          c           |   3 |
+      +----------------------+-----+
+    EOF
   end
   
 end
