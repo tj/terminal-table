2026fc3c2adede6e8240d5ef79ab6eea3fbfabb5
TJ Holowaychuk <tj@vision-media.ca>
2009-03-14 22:25:43 -0700

    Allowing nil to be passed to table for headings

--- lib/terminal-table/import.rb
+++ lib/terminal-table/import.rb
@@ -48,6 +48,6 @@ module Kernel
   #
   
   def table headings = [], *rows, &block
-    Terminal::Table.new :headings => headings, :rows => rows, &block
+    Terminal::Table.new :headings => headings.to_a, :rows => rows, &block
   end
 end
