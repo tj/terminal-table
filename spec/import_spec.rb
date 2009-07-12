f68d4ef6ac92d3f408b738bd7e22675967c6cd36
Scott Taylor <scott@railsnewbie.com>
2009-07-13 05:42:10 +0800

    Fix specs so that they will run without the gem installed.
    Signed-off-by: TJ Holowaychuk <tj@vision-media.ca>

--- spec/import_spec.rb
+++ spec/import_spec.rb
@@ -1,13 +1,11 @@
-
-require 'terminal-table/import'
+require File.expand_path(File.dirname(__FILE__) + "/spec_helper")
+require "terminal-table/import"
 
 describe Kernel do
-  
   describe "#table" do
     it "should allow creation of a terminal table" do
       table(['foo', 'bar'], ['a', 'b'], [1, 2]).
       should be_instance_of(Terminal::Table)
     end
   end
-
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
 
-require 'terminal-table'
+require File.expand_path(File.dirname(__FILE__) + "/../lib/terminal-table")
 
 class String
   def deindent
