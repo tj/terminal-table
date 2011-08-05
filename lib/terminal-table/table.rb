9510aaf2bf265802819f88d421928c69ef7d2ea6
Niko Dittmann <mail+git@niko-dittmann.com>
2011-08-05 11:18:40 +0200

    ! fix for https://github.com/visionmedia/terminal-table/issues/6

--- lib/terminal-table/table.rb
+++ lib/terminal-table/table.rb
@@ -247,6 +247,14 @@ module Terminal
       raise Error, 'your table needs some rows'
     end
     
+    def number_of_columns
+      raise Error, 'your table needs some rows' if rows.empty?
+      
+      rows.first.inject(0) do |mem,cell|
+        mem += (cell.is_a?(Hash) && !cell[:colspan].nil?) ? cell[:colspan] : 1
+      end
+    end
+    
     ##
     # Align column _n_ to the given _alignment_ of :center, :left, or :right.
     
