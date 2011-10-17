5aa4b48e72c7d72b5d3e4606171ce4b3b9768a97
René Föhring <rf@bamaru.de>
2011-10-18 00:24:11 +0200

    Add documentation to Style class

--- lib/terminal-table/style.rb
+++ lib/terminal-table/style.rb
@@ -1,13 +1,32 @@
 
 module Terminal
   class Table
+    # A Style object holds all the formatting information for a Table object
+    # 
+    # To create a table with a certain style, use either the constructor 
+    # option <tt>:style</tt>, the Table#style object or the Table#style= method
+    #
+    # All these examples have the same effect:
+    # 
+    #     # by constructor
+    #     @table = Table.new(:style => {:padding_left => 2, :width => 40})
+    #
+    #     # by object
+    #     @table.style.padding_left = 2
+    #     @table.style.width = 40
+    #
+    #     # by method
+    #     @table.style = {:padding_left => 2, :width => 40}
+    # 
+    # To set a default style for all tables created afterwards use Style.defaults=
+    # 
+    #     Terminal::Table::Style.defaults = {:width => 80}
+    #
     class Style
       @@defaults = {
         :border_x => "-", :border_y => "|", :border_i => "+",
         :padding_left => 1, :padding_right => 1
       }
-      ##
-      # Style options.
       
       attr_accessor :border_x
       attr_accessor :border_y
@@ -18,8 +37,6 @@ module Terminal
       
       attr_accessor :width
       
-      ##
-      # Initialize with _options_.
       
       def initialize options = {}
         apply self.class.defaults.merge(options)
@@ -28,6 +45,7 @@ module Terminal
       def apply options
         options.each { |m, v| __send__ "#{m}=", v }
       end
+      
       class << self
         def defaults
           @@defaults
